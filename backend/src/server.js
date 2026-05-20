f18cc82ddc655e4770e10f52af3953a4084dba8c
Rishabh Singh <rishabhsinghnitian231@gmail.com>
2026-05-20 14:07:05 +0530

    setup: initialize frontend and backend architecture

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+
+
+const port = process.env.PORT;
+
+const initializeConnection = async ()=>{
+    try{
+        await Promise.all([main()]);
+        console.log("DB connected");
+
+        app.listen(port,()=>{
+            console.log("Server is listening at port : ",port);
+        });
+    }
+    catch(err){
+        console.log("Error : ",err.message);
+    }
+}
+
+initializeConnection();
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen bg-slate-900 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Rentora
+      </h1>
+    </div>
+  );
+}
+
+export default App;
