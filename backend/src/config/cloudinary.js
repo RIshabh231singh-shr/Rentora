025ec5e7fb42784ac03b03e97012681c21d18d1a
Rishabh Singh <rishabhsinghnitian231@gmail.com>
2026-07-19 21:54:42 +0530

    fix(backend): use correct cloudinary environment variables ds3dyuneg

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,10 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 
-if (process.env.CLOUDINARY_CLOUD_NAME && process.env.CLOUDINARY_API_KEY && process.env.CLOUDINARY_API_SECRET) {
+if (process.env.CLOUDINARY_NAME && process.env.CLOUDINARY_KEY && process.env.CLOUDINARY_SECRET) {
     cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET
     });
     console.log("Cloudinary configured successfully.");
 } else {
